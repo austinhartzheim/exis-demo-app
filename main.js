57ea1929a2c76e4d40fb010ca498fa4a0fa168db
Austin <austin@austinhartzheim.me>
2016-04-09 18:59:36 +0000

    Added get-city function.

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ riffle.setLogLevelInfo();
 
 var domain = riffle.Domain(process.env.DOMAIN);
 
-var city_elevations = [
-    [46.3338, 48.0219],
-    [41.3317, 19.8172],
-    [48.2092, 16.3728],
-    [53.9678, 27.5766]
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var cities = [
+    [29.3900, 91.0700],
+    [12.6530, 7.9864],
+    [19.0421, 65.2559],
+    [0.2295, 78.5243]
 ];
 
 domain.onJoin = function() {
@@ -22,9 +26,9 @@ domain.onJoin = function() {
         return [Math.random()*180-90, Math.random()*180-90];
     }));
 
-    this.register("get-city-elevation", riffle.want(function(i) {
-        return city_elevations[i];
-    }, Number));
+    this.register("get-city", riffle.want(function() {
+        return cities[randInt(0, cities.length - 1)];
+    }));
 };
 
 domain.join();
