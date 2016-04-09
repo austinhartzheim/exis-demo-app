5ba3bd4dc2081d8a4742fb15b06632df71d80487
Austin <austin@austinhartzheim.me>
2016-04-09 18:20:41 +0000

    Added a function to get a city point by elevation index.

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ riffle.setLogLevelInfo();
 
 var domain = riffle.Domain(process.env.DOMAIN);
 
+var city_elevations = [
+    [46.3338, 48.0219],
+    [41.3317, 19.8172],
+    [48.2092, 16.3728],
+    [53.9678, 27.5766]
+];
+
 domain.onJoin = function() {
     this.register("echo", riffle.want(function(msg) {
         console.log("Echo: " + msg);
@@ -13,7 +20,11 @@ domain.onJoin = function() {
 
     this.register("get-point", riffle.want(function() {
         return [Math.random()*180-90, Math.random()*180-90];
-    }));  
+    }));
+
+    this.register("get-vity-elevation", riffle.want(function(i) {
+        return city_elevations[i];
+    }, Number));
 };
 
-domain.join()
+domain.join();
